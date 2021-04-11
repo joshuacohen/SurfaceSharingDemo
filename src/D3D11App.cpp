1db389c1245d4d417981ed7c25504eb5540aa718
Josh Cohen <joshua_cohen@me.com>
2021-04-10 20:07:23 -0400

    Shared surface creation and handle retrieval

--- src/D3D11App.cpp
+++ src/D3D11App.cpp
@@ -9,8 +9,7 @@ inline void ThrowOnError(HRESULT hr) {
 }
 
 void D3D11App::Init(HWND hwnd) {
-	DXGI_SWAP_CHAIN_DESC scd {0};
-	scd = {
+	DXGI_SWAP_CHAIN_DESC scd = {
 		DXGI_MODE_DESC {
 			800,
 			600,
@@ -30,6 +29,16 @@ void D3D11App::Init(HWND hwnd) {
 		true
 	};
 
+	D3D_FEATURE_LEVEL features[] = {
+		D3D_FEATURE_LEVEL_9_1,
+		D3D_FEATURE_LEVEL_9_2,
+		D3D_FEATURE_LEVEL_9_3,
+		D3D_FEATURE_LEVEL_10_0,
+		D3D_FEATURE_LEVEL_10_1,
+		D3D_FEATURE_LEVEL_11_0,
+		D3D_FEATURE_LEVEL_11_1,
+	};
+
 	ThrowOnError(CreateDXGIFactory1(IID_PPV_ARGS(&factory)));
 
 	ID3D11Device* device0;
@@ -39,19 +48,47 @@ void D3D11App::Init(HWND hwnd) {
 		D3D_DRIVER_TYPE_HARDWARE,
 		nullptr,
 		D3D11_CREATE_DEVICE_DEBUG,
-		nullptr,
-		0,
+		features,
+		7,
 		D3D11_SDK_VERSION,
 		&scd,
 		&swapChain,
 		&device0,
 		nullptr,
-		&context
-	));
+		&context));
 
 	ThrowOnError(device0->QueryInterface<ID3D11Device2>(&device));
 	ThrowOnError(swapChain->GetBuffer(0, IID_PPV_ARGS(&renderTarget)));
-	ThrowOnError(device->CreateRenderTargetView(renderTarget, nullptr, &rtv));
+
+	D3D11_TEXTURE2D_DESC ss {
+		800,
+		600,
+		1,
+		1,
+		DXGI_FORMAT_B8G8R8A8_UNORM,
+		DXGI_SAMPLE_DESC {
+			1,
+			0
+		},
+		D3D11_USAGE_DEFAULT,
+		D3D11_BIND_SHADER_RESOURCE | D3D11_BIND_RENDER_TARGET,
+		0,
+		D3D11_RESOURCE_MISC_SHARED_KEYEDMUTEX | D3D11_RESOURCE_MISC_SHARED_NTHANDLE
+	};
+
+	ThrowOnError(device->CreateTexture2D(&ss, nullptr, &sharedSurface));
+
+	IDXGIResource1* sharedResource = nullptr;
+	ThrowOnError(sharedSurface->QueryInterface(IID_PPV_ARGS(&sharedResource)));
+
+	ThrowOnError(sharedResource->CreateSharedHandle(
+		nullptr, 
+		DXGI_SHARED_RESOURCE_READ | DXGI_SHARED_RESOURCE_WRITE,
+		L"Shared Surface",
+		&sharedSurfaceHandle
+	));
+
+	//ThrowOnError(device->CreateRenderTargetView(renderTarget, nullptr, &rtv));
 }
 
 bool D3D11App::Update() {
--- src/inc/D3D11App.h
+++ src/inc/D3D11App.h
@@ -11,9 +11,9 @@ struct ID3D11RenderTargetView;
 
 class D3D11App {
 public:
-void Init(HWND hwnd);
-bool Update();
-void Shutdown();
+	void Init(HWND hwnd);
+	bool Update();
+	void Shutdown();
 
 protected:
 	IDXGIFactory1* factory;
@@ -21,7 +21,10 @@ protected:
 	ID3D11Device2* device;
 	ID3D11DeviceContext* context;
 	ID3D11Texture2D* renderTarget;
+	ID3D11Texture2D* sharedSurface;
 	ID3D11RenderTargetView* rtv;
+
+	HANDLE sharedSurfaceHandle;
 private:
 
 };
