e8cf5dd0e59a6f0715a2bb9641607dac7c8b11f0
Josh Cohen <jocoh@microsoft.com>
2021-04-14 08:19:26 -0700

    12: depth buffer creation

--- src/SharedSurface12/D3D12App.cpp
+++ src/SharedSurface12/D3D12App.cpp
@@ -33,19 +33,58 @@ void D3D12App::Init(std::wstring surfaceGuidStr, std::wstring fenceGuidStr) {
 		0
 	};
 
+	D3D12_DESCRIPTOR_HEAP_DESC dh_dsv {
+		D3D12_DESCRIPTOR_HEAP_TYPE_RTV,
+		1,
+		D3D12_DESCRIPTOR_HEAP_FLAG_NONE,
+		0
+	};
+
 	D3D12_RENDER_TARGET_VIEW_DESC rtvd {
 		DXGI_FORMAT_B8G8R8A8_UNORM,
 		D3D12_RTV_DIMENSION_TEXTURE2D,
 		0
 	};
 
+	D3D12_RESOURCE_DESC depthBufferDesc {
+		D3D12_RESOURCE_DIMENSION_TEXTURE2D,
+		0,
+		800,
+		600,
+		1,
+		0,
+		DXGI_FORMAT_D24_UNORM_S8_UINT,
+		DXGI_SAMPLE_DESC {
+			1,
+			0
+		},
+		D3D12_TEXTURE_LAYOUT_UNKNOWN,
+		D3D12_RESOURCE_FLAG_ALLOW_DEPTH_STENCIL
+	};
+
+	D3D12_HEAP_PROPERTIES implicitHeapProperties {
+		D3D12_HEAP_TYPE_DEFAULT,
+		D3D12_CPU_PAGE_PROPERTY_UNKNOWN,
+		D3D12_MEMORY_POOL_UNKNOWN,
+		0,
+		0
+	};
+
+	D3D12_CLEAR_VALUE depthBufferClearVal {
+		DXGI_FORMAT_D24_UNORM_S8_UINT,
+		1.0f,
+		0
+	};
+
+	ComPtr<ID3D12Debug> debugLayer;
+	ThrowOnError(D3D12GetDebugInterface(IID_PPV_ARGS(&debugLayer)));
+	debugLayer->EnableDebugLayer();
+
 	ThrowOnError(D3D12CreateDevice(nullptr, D3D_FEATURE_LEVEL_12_1, IID_PPV_ARGS(&device)));
 	ThrowOnError(device->CreateCommandQueue(&cq, IID_PPV_ARGS(&commandQueue)));
 	ThrowOnError(device->CreateCommandAllocator(D3D12_COMMAND_LIST_TYPE_DIRECT, IID_PPV_ARGS(&commandAllocator)));
 	ThrowOnError(device->CreateDescriptorHeap(&dh_rtv, IID_PPV_ARGS(&rtvHeap)));
-
-	rtvHandle = rtvHeap->GetCPUDescriptorHandleForHeapStart();
-	device->CreateRenderTargetView(sharedSurface.Get(), &rtvd, rtvHandle);
+	ThrowOnError(device->CreateDescriptorHeap(&dh_dsv, IID_PPV_ARGS(&dsvHeap)));
 
 	ThrowOnError(device->CreateCommandList(
 		0,
@@ -53,6 +92,20 @@ void D3D12App::Init(std::wstring surfaceGuidStr, std::wstring fenceGuidStr) {
 		commandAllocator.Get(),
 		nullptr,
 		IID_PPV_ARGS(&commandList)
+	));
+
+
+	rtvHandle = rtvHeap->GetCPUDescriptorHandleForHeapStart();
+	device->CreateRenderTargetView(sharedSurface.Get(), &rtvd, rtvHandle);
+	dsvHandle = dsvHeap->GetCPUDescriptorHandleForHeapStart();
+
+	ThrowOnError(device->CreateCommittedResource(
+		&implicitHeapProperties,
+		D3D12_HEAP_FLAG_NONE,
+		&depthBufferDesc,
+		D3D12_RESOURCE_STATE_DEPTH_WRITE,
+		&depthBufferClearVal,
+		IID_PPV_ARGS(&depthBuffer)
 	));
 
 	commandList->OMSetRenderTargets(1, &rtvHandle, true, nullptr);
--- src/SharedSurface12/inc/D3D12App.h
+++ src/SharedSurface12/inc/D3D12App.h
@@ -18,9 +18,12 @@ protected:
 	ComPtr<ID3D12CommandAllocator> commandAllocator;
 	ComPtr<ID3D12GraphicsCommandList> commandList;
 	ComPtr<ID3D12DescriptorHeap> rtvHeap;
+	ComPtr<ID3D12DescriptorHeap> dsvHeap;
 	ComPtr<ID3D12Resource> sharedSurface;
+	ComPtr<ID3D12Resource> depthBuffer;
 	ComPtr<ID3D12Fence> sharedFence;
 
 	D3D12_CPU_DESCRIPTOR_HANDLE rtvHandle;
+	D3D12_CPU_DESCRIPTOR_HANDLE dsvHandle;
 	HANDLE cpuFenceEvent;
 };
